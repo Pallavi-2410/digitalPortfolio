97b5cf459d6c8bd8d44eb0ba63f9fa93a28a7bce
Pallavi-2410 <patel.pallavi1819@gmail.com>
2025-02-15 02:11:54 +0530

    Project added

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,18 +1,20 @@
 import React from "react";
-import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaEnvelope, FaExternalLinkAlt } from "react-icons/fa";
 import { motion } from "framer-motion";
 import "../styles/Portfolio.css";
 // import photo from "../images/profileImg.jpg"
-import logo from "../images/Logo-removebg.png"
+import logo from "../images/girlLogo.gif"
 import profilePic from "../images/profileImg.jpg"
 import Pallavi_Patidar_Resume from "../resume/PallaviPatidar-FrontendDeveloper.pdf"
 import html from "../images/techLogo/html.png"
 import css from "../images/techLogo/css.png"
 import js from "../images/techLogo/js.png"
 import react from "../images/techLogo/react.png"
+import chakraUi from "../images/techLogo/chakraUi.png"
 import git from "../images/techLogo/git.png"
 import github from "../images/techLogo/github.png"
 import netlify from "../images/techLogo/netlify.png"
+import projectBanner from "../images/projectBanner.png"
 
 export default function Portfolio() {
     const handleResumeDownload = () => {
@@ -91,34 +93,59 @@ export default function Portfolio() {
                 <section id="skills" className="skills">
                     <h2 className="section-title">Technical Skills</h2>
                     <div className="skills-list">
-                        <div className="skill-item"><img src={html} alt="HTML" /></div>
-                        <div className="skill-item"><img src={css} alt="css" /></div>
-                        <div className="skill-item"><img src={js} alt="js" /></div>
-                        <div className="skill-item"><img src={react} alt="react" /></div>
-                        <div className="skill-item"><img src={github} alt="github" /></div>
-                        <div className="skill-item"><img src={git} alt="git" /></div>
-                        <div className="skill-item"><img src={netlify} alt="netlify" /></div>                        
+                        <div className="skill-item" data-skill="HTML"><img src={html} alt="HTML" /></div>
+
+                        <div className="skill-item" data-skill="CSS"  ><img src={css} alt="css" /></div>
+
+                        <div className="skill-item" data-skill="JS" ><img src={js} alt="js" /></div>
+
+                        <div className="skill-item" data-skill="React"><img src={react} alt="react" /></div>
+
+                        <div className="skill-item" data-skill="Chakra UI"><img src={chakraUi} alt="chakraUi"/></div>
+
+                        <div className="skill-item" data-skill="Github"><img src={github} alt="github"/></div>
+
+                        <div className="skill-item" data-skill="Git" ><img src={git} alt="git" /></div>
+
+                        <div className="skill-item" data-skill="Netlify"><img src={netlify} alt="netlify"  /></div>                        
                     </div>
                 </section>
 
                 {/* Projects Section */}
                 <section id="projects" className="project-banner">
                     <h2>Projects</h2>
-                    <div className="project-list">
-                        <div className="project-item">Chai-iayah</div>
+                    <div className="project-detail">
+                        <h3>Chai-iayah</h3>
+                        <p>A celebration of chai culture, inspired by the warm and inviting call of local chaiwalas that resonates in every corner of India. This platform is dedicated to the love of chai, coffee, and delightful snacks that bring people together. Explore a world where every sip tells a story steeped in tradition, healing, and comfort. From the rich aroma of freshly brewed chai to a variety of accompaniments like biscuits, snacks, and more, we aim to create an experience that feels like home. Find your love for food and discover the essence of India’s chai heritage, brewed to perfection just for you.</p>
+                        
+                        <div className="project-tech-cont">
+                            <div className="project-tech"><img src={html} alt="HTML" /></div>
+                            <div className="project-tech" ><img src={css} alt="css" /></div>
+                            <div className="project-tech" ><img src={js} alt="js" /></div>
+                        </div>
+                        <br />
                     </div>
+                    <div className="project-list">
+                        <div className="project-item">
+                            <img src={projectBanner} alt="projectBanner" />
+                    
                     <div className="project-links">
-                        <a href="https://frontend-link.com" target="_blank" rel="noopener noreferrer">Frontend</a>
-                        <a href="https://backend-link.com" target="_blank" rel="noopener noreferrer">Backend</a>
+                                <a href="https://pallavi-2410.github.io/Chai-iayah/" target="_blank" rel="noopener noreferrer" >Live <FaExternalLinkAlt className="icon" /></a>
+                                <a href="https://github.com/Pallavi-2410/Chai-iayah" target="_blank" rel="noopener noreferrer" >Github <FaGithub className="icon" /></a>
+                            </div>
+                        </div>
                     </div>
                 </section>
 
                 {/* Contact Section */}
                 <section id="contact" className="contact">
                     <h2 className="section-title">Connect with Me</h2>
-                    <p>Phone: +91 8602241866</p>
-                    <p>Email: patel.pallavi1819@gmail.com</p>
-                    <p>Location: Pune, India</p>
+                    <div className="contact-details">
+                        <p>Phone: +91 8602241866</p>
+                        <p>Email: patel.pallavi1819@gmail.com</p>
+                        <p>Location: Pune, India</p>
+                    </div>
+                    
                     <div className="contact-links">
                         <motion.a href="https://github.com/yourgithub" target="_blank" rel="noreferrer" whileHover={{ scale: 1.3 }}>
                             <FaGithub className="icon" />
